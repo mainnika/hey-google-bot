e06990c1c68211250f0b819f26813f211c276f6e
mainnika <nikita@tokarch.uk>
2017-04-02 21:04:50 +0500

    catch errors from handlers

--- lib/create-command.ts
+++ lib/create-command.ts
@@ -6,5 +6,10 @@ import { Command } from './command';
 
 export const createCommand =
 	<T extends Command>(ctor: { new (ctx: Context): T }): Middleware =>
-		(ctx: Context) =>
-			new ctor(ctx).exec();
+		async (ctx: Context) => {
+			try {
+				await new ctor(ctx).exec();
+			} catch (err) {
+				console.error(err);
+			};
+		}
